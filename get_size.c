24292a6a8654bd375c4df13fab5a3ba88de4fd0d
lihle03 <lanmaljrlm>
2023-08-19 12:44:12 -0700

    get_size.c

--- get_size.c
+++ get_size.c
@@ -4,7 +4,7 @@
  * get_size - calculates the size of the cast on he argument.
  * @format: the formatted string prints the argument.
  * @i: list all the arguments printed.
- * 
+ *
  * Return: Presition (Success)
  */
 int get_size(const char *format, int *i)
@@ -13,12 +13,12 @@ int get_size(const char *format, int *i)
 	int size = 0;
 
 	if (format[currnt_i] == 'l')
-			size = S_LONG;
+		size = S_LONG;
 	else if (format[currnt_i] == 'h')
-			size = S_SHORT;
+		size = S_SHORT;
 	if (size == 0)
 		*i = currnt_i - 1;
-	else 
+	else
 		*i = currnt_i;
 
 	return (size);
