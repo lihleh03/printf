3717f87a9165e70fba27a393564d79629d1ca27d
lihleh03 <ntulicebelihle03@gmail.com>
2023-08-21 11:22:07 -0700

    main.c

--- main.c
+++ main.c
@@ -9,13 +9,13 @@
  */
 int main(void)
 {
-    int len;
-    int len2;
-    unsigned int ui;
-    void *addr;
+int len;
+int len2;
+unsigned int ui;
+void *addr;
 
-    len = _printf("Let's try to printf a simple sentence.\n");
-    len2 = printf("Let's try to printf a simple sentence.\n");
+len = _printf("Let's try to printf a simple sentence.\n");
+len2 = printf("Let's try to printf a simple sentence.\n");
     ui = (unsigned int)INT_MAX + 1024;
     addr = (void *)0x7ffe637541f0;
     _printf("Length:[%d, %i]\n", len, len);
